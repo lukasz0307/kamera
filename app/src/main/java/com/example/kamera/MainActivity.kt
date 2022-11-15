2fa80f63cee05f6dd66ccb2d091aac52dee0c973
sztoslol <kubuszeski05@wp.pl>
2022-11-15 14:45:03 +0100

    commit

--- app/src/main/java/com/example/kamera/MainActivity.kt
+++ app/src/main/java/com/example/kamera/MainActivity.kt
@@ -1,24 +1,42 @@
 package com.example.kamera
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.location.LocationManager
+import android.location.LocationRequest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import com.example.kamera.databinding.ActivityMainBinding
-import java.util.jar.Manifest
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+//import java.util.jar.Manifest
 //import kotlinx.android.synthetic.main.activity.main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
+
+private var PERMISSION_ID=1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding=ActivityMainBinding.inflate(layoutInflater)
+        val binding= com.example.kamera.databinding.ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.button.isEnabled=false
+
+        fusedLocationProviderClient=LocationServices.getFusedLocationProviderClient(this)
 
         if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.CAMERA)!=PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA),111)
@@ -32,8 +50,51 @@ class MainActivity : AppCompatActivity() {
             startActivityForResult(i,101)
 
         }
+
         //setContentView(R.layout.activity_main)
     }
+    private fun getLastLocation(){
+        if (CheckPermission()){
+            if (isLocationEnabled()){
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
+                    var location = task.result
+                    if (location==null){
+
+                    }else{
+                        val textView: TextView=findViewById(R.id.szerokosc)as TextView
+                        textView.setText("Your Current Coordinates are: \nLat: "+location.latitude+";Long: "+location.longitude)
+                    }
+                }
+            }else{
+                Toast.makeText(this,"Proszę udzielić zgodę na dostęp do lokalizacji",Toast.LENGTH_LONG).show()
+            }
+        }else{
+            RequestPermission()
+        }
+    }
+
+
+    private fun CheckPermission():Boolean{
+        if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED||
+                ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+            return true
+        }
+
+        return false
+    }
+
+    private fun RequestPermission(){
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION),PERMISSION_ID)
+
+    }
+
+    private fun isLocationEnabled():Boolean{
+        var locationManager: LocationManager=getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)||locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -56,6 +117,11 @@ class MainActivity : AppCompatActivity() {
             val binding=ActivityMainBinding.inflate(layoutInflater)
             setContentView(binding.root)
             binding.button.isEnabled=true
+            if (requestCode==PERMISSION_ID){
+                if(grantResults.isNotEmpty()&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    Log.d("Debug:","Musisz wyrazić zgode")
+                }
+            }
         }
     }
 }
