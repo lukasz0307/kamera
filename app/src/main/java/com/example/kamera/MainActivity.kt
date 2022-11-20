007e0401713f0974549426416c8d818719ac981c
aniao <aniao@LAPTOP-OC2DI8K7>
2022-11-20 21:56:34 +0100

    do oceny

--- app/src/main/java/com/example/kamera/MainActivity.kt
+++ app/src/main/java/com/example/kamera/MainActivity.kt
@@ -5,26 +5,22 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.location.Geocoder
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
-import android.location.LocationRequest
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Looper
 import android.provider.MediaStore
-import android.util.Log
 import android.view.View
 import android.widget.Switch
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import com.example.kamera.databinding.ActivityMainBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import java.util.*
 
 //import java.util.jar.Manifest
 //import kotlinx.android.synthetic.main.activity.main.*
@@ -34,20 +30,15 @@ class MainActivity : AppCompatActivity() ,LocationListener{
     private lateinit var locationManager: LocationManager
     private lateinit var longitude: TextView
     private lateinit var latitude: TextView
+    private lateinit var dostawca: TextView
+    private lateinit var gps: Switch
     private val locationPermissionCode = 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding= com.example.kamera.databinding.ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.button.isEnabled=false
-        val gps:Switch=findViewById(R.id.gps)
-        gps.setOnClickListener {
-           longitude=findViewById(R.id.dlugosc)
-           latitude =findViewById(R.id.szerokosc)
-            //longitude.visibility=View.INVISIBLE
-            //visibility=View.INVISIBLE
-        }
-
+        hide_Location()
         getLocation()
 
         if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.CAMERA)!=PackageManager.PERMISSION_GRANTED){
@@ -60,6 +51,7 @@ class MainActivity : AppCompatActivity() ,LocationListener{
         binding.button.setOnClickListener {
             var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivityForResult(i,101)
+
 
         }
 
@@ -74,6 +66,8 @@ class MainActivity : AppCompatActivity() ,LocationListener{
             var obrazek:Bitmap?
             obrazek=data?.getParcelableExtra<Bitmap>("data")
             bindnig.image.setImageBitmap(obrazek)
+            getLocation()
+            hide_Location()
         }
     }
 
@@ -85,10 +79,34 @@ class MainActivity : AppCompatActivity() ,LocationListener{
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
     }
     override fun onLocationChanged(location: Location) {
+        val geocoder = Geocoder(this@MainActivity, Locale.getDefault())
+        val addresses = geocoder.getFromLocation(location.latitude, location.longitude, 1)
+        val country = addresses[0].getAddressLine(0)
+        dostawca=findViewById(R.id.dostawca)
         latitude = findViewById(R.id.szerokosc)
         longitude = findViewById(R.id.dlugosc)
-        latitude.text = "Latitude: " + location.latitude
-        longitude.text = "Longitude: " + location.longitude
+        latitude.text = "Szerokość: " + location.latitude
+        longitude.text = "Długość: " + location.longitude
+        dostawca.text="Dostawca: \n"+country
+    }
+     fun hide_Location(){
+         gps=findViewById(R.id.gps)
+         gps.setOnClickListener {
+             dostawca=findViewById(R.id.dostawca)
+             latitude = findViewById(R.id.szerokosc)
+             longitude = findViewById(R.id.dlugosc)
+             if(longitude.isVisible&&latitude.isVisible&&dostawca.isVisible){
+                 longitude.visibility=View.INVISIBLE
+                 latitude.visibility=View.INVISIBLE
+                 dostawca.visibility=View.INVISIBLE
+             }else{
+                 longitude.visibility=View.VISIBLE
+                 latitude.visibility=View.VISIBLE
+                 dostawca.visibility=View.VISIBLE
+
+             }
+
+         }
     }
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
